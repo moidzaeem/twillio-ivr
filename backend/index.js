ea82f041570bd4a463f55e7513c65f282be2ed61
TMS <moidzaeem@gmail.com>
2025-06-16 02:08:12 +0500

    done

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ try {
   });
 
   // Main API routes
-  app.use('/api/twilio', twilioRoutes);
+  // app.use('/api/twilio', twilioRoutes);
 
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
